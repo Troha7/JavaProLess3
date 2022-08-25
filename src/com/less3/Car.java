4d66f0b17abeac1ced1cfc211b9551049963e306
Dmytro Trotsenko <trohae7@gmail.com>
2022-08-25 12:55:15 +0300

    Add class Car

---
+++ src/com/less3/Car.java
@@ -0,0 +1,15 @@
+package com.less3;
+
+public class Car {
+    public Car() {
+    }
+
+    public void start(){
+        startElectricity();
+        startCommand();
+        startFuelSystem();
+    }
+    private void startElectricity(){}
+    private void startCommand(){}
+    private void startFuelSystem(){}
+}
