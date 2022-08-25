2850d0a4861d677030e98b7656f37b5f2e2d2176
Dmytro Trotsenko <trohae7@gmail.com>
2022-08-25 16:14:40 +0300

    Fixed calling SameName objects

--- src/com/less3/Main.java
+++ src/com/less3/Main.java
@@ -1,14 +1,12 @@
 package com.less3;
 
-import com.less3.sameName.SameName;
-
 public class Main {
     public static void main(String[] args) {
         Employee employee = new Employee
                 ("Dmytro Trotsenko","Software developer","trohae7@gmail.com","123",32);
 
         SameName sameName1 = new SameName();
-        SameName sameName2 = new SameName();
+        com.less3.sameName.SameName sameName2 = new com.less3.sameName.SameName();
 
         Car car = new Car();
         car.start();
