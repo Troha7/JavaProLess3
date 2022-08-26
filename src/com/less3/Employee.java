bf7930800435fe965152d76cebc38d14fafb02c1
Dmytro Trotsenko <trohae7@gmail.com>
2022-08-26 23:45:02 +0300

    Add getters and setters in Employee class

--- src/com/less3/Employee.java
+++ src/com/less3/Employee.java
@@ -7,11 +7,46 @@ public class Employee {
     private String phone;
     private int age;
 
-    public Employee(String fullName, String position, String email, String phone, int age) {
+    public Employee() {
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
         this.fullName = fullName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
         this.position = position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
         this.phone = phone;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
         this.age = age;
     }
 }
