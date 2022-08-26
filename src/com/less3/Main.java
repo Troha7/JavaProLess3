a89553b927de77bd19522a8e89377637a341a855
Dmytro Trotsenko <trohae7@gmail.com>
2022-08-26 23:46:25 +0300

    Fixed Main class

--- src/com/less3/Main.java
+++ src/com/less3/Main.java
@@ -2,8 +2,20 @@ package com.less3;
 
 public class Main {
     public static void main(String[] args) {
-        Employee employee = new Employee
-                ("Dmytro Trotsenko","Software developer","trohae7@gmail.com","123",32);
+
+        Employee employee = new Employee();
+
+        employee.setFullName("Dmytro Trotsenko");
+        employee.setPosition("Software developer");
+        employee.setEmail("trohae7@gmail.com");
+        employee.setPhone("123");
+        employee.setAge(32);
+
+        System.out.println("Full name: " + employee.getFullName());
+        System.out.println("Position: " + employee.getPosition());
+        System.out.println("Email: " + employee.getEmail());
+        System.out.println("Phone: " + employee.getPhone());
+        System.out.println("Age: " + employee.getAge());
 
         SameName sameName1 = new SameName();
         com.less3.sameName.SameName sameName2 = new com.less3.sameName.SameName();
